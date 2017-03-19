d956ba50daad1777a3d97facae603605ea43ae8f
Cameron Moore <cammoo37@gmail.com>
2017-03-18 21:01:14 -0500

    Add paddle movement, projectile firing, change camera size

---
+++ Pong Invaders/Assets/Scripts/Player/clonerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class clonerScript : MonoBehaviour {
+
+public GameObject playerProjectile;
+GameObject playerProjectileClone;
+
+	// Update is called once per frame
+	void Update () {
+		// projectile firing
+		if (Input.GetButton("Fire1")) {
+			playerProjectileClone = Instantiate(playerProjectile, transform.position,Quaternion.identity) as GameObject;
+		}
+	}
+}
---
+++ Pong Invaders/Assets/Scripts/Player/playerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerControl : MonoBehaviour {
+	
+	float m_speed = 13;
+	GameObject playerProjectileClone;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {		
+		
+		// movement
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"),0);
+        transform.position += move * m_speed * Time.deltaTime;
+				
+	}
+}
