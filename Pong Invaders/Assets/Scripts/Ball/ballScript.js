38fe5ab0499592e78a6abddf73011e4a29a60dac
Cameron Moore <cammoo37@gmail.com>
2017-03-20 00:29:08 -0500

    Oh my Lord, Unity is rough.

--- Pong Invaders/Assets/Scripts/Ball/ballScript.js
+++ Pong Invaders/Assets/Scripts/Ball/ballScript.js
@@ -7,14 +7,36 @@ var velX = 0f;
 var velY = 0f;
 var direction = 0f;
 
+var ballRigidBody;
+
 function Start () {
+	ballRigidBody = gameObject.GetComponent(Rigidbody2D);
 	direction = -1f;
 	velY = startSpeedY * direction;
 }
 
 function Update () {
 	// Movement update
+	velY = velY * direction;
+	/*
 	gameObject.transform.position.x += velX;
 	gameObject.transform.position.y += velY;
+	*/
 	
+}
+
+function OnTriggerEnter2D(other: Collider2D) {
+	
+	
+	if(other.gameObject.name == "RightWall" || other.gameObject.name == "LeftWall") {
+		velX = -velX;
+	}
+	
+	if(other.gameObject.name == "TopWall" || other.gameObject.name == "BotWall")  {
+		direction = -direction;
+	}
+	
+	if(other.gameObject.name == "obj_player") {
+		direction = -direction;
+	}
 }
