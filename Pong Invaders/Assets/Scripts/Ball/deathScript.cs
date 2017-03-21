9d510411ef7f7dde8797503e63d24a3b5b959c48
Jonah Knickles <jknick1@lsu.edu>
2017-03-21 00:14:47 -0500

    You can die now.
    Ball resets when it goes out of bounds.

---
+++ Pong Invaders/Assets/Scripts/Ball/deathScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class deathScript : MonoBehaviour {
+
+public GameObject ContainmentPrefab;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+void OnTriggerEnter2D(Collider2D other){
+
+    if (other.gameObject.name == "prefab_ball(Clone)") {
+
+        Destroy (other.gameObject);
+        GameObject Containment = Instantiate(ContainmentPrefab, new Vector2(0,0), Quaternion.identity) as GameObject;
+			}
+}
+}
