5a57f5a9ccfd64fa2d96b3fa17a7c557c321a7c4
Mayor-Cam <cammoo37@gmail.com>
2017-03-21 02:08:22 -0500

    Changed ballScript for dynamic paddle collision

--- Pong Invaders/Assets/Scripts/Ball/ballScript.cs
+++ Pong Invaders/Assets/Scripts/Ball/ballScript.cs
@@ -4,21 +4,49 @@ using UnityEngine;
 
 public class ballScript : MonoBehaviour {
 	
+	public GameObject obj_player;
+	public GameObject obj_player2;
+	private playerControl playerControl;
+	private playerControl2 playerControl2;
+	Rigidbody2D ballRigidBody;
+	
+	
+	/*
 	float minSpeedY = 0.5f;
 	float maxSpeedY = 5f;
 	float velX = 0f;
 	float velY = 0f;
 	//double velocity = new Vector3(velX,velY,0);
+	*/
 	
+	void Awake () {
+		ballRigidBody = gameObject.GetComponent<Rigidbody2D>();
+		playerControl = GetComponent<playerControl>();
+		playerControl2 = GetComponent<playerControl2>();
+	}
 	
 	void Start () {
-		velY = -minSpeedY;
+	//	velY = -minSpeedY;
 	}
 	
 	
 	void Update () {
 		// Movement update
 		// velocity =(velX,velY,0);
-		transform.position = new Vector3(0,velY,0);
+	//	transform.position = new Vector3(0,velY,0);
+	}
+	
+	void OnCollisionEnter2D(Collision2D coll) {
+		print("collisionEnter2D");
+		
+		// Imparts horizontal force using velX of paddle
+		if (coll.gameObject.name == "obj_player") {
+			print("player 1 collision");
+			ballRigidBody.AddForce(new Vector2(playerControl.velX,0));
+		}
+		else if (coll.gameObject.name == "obj_player2") {
+			print("player 2 collision");
+			ballRigidBody.AddForce(new Vector2(playerControl2.velX,0));
+		}
 	}
 }
