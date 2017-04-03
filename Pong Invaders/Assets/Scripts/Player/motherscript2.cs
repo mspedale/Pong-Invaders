a969adc55bd6f3fe1a4f3fc51d93fd5ef34f6720
Jonah Knickles <jknick1@lsu.edu>
2017-04-02 22:15:32 -0500

    Added HP and Shield functionality

---
+++ Pong Invaders/Assets/Scripts/Player/motherscript2.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class motherscript2 : MonoBehaviour {
+    
+    AudioSource scoreSound;
+    public GameObject ContainmentPrefab;
+    int hp=10;
+    bool shield=true;
+    void OnTriggerEnter2D(Collider2D other){
+        
+        if(shield==false){
+            print(hp);
+        hp=hp-1;
+        if(hp<1){
+            Destroy (gameObject);
+
+        }
+        }
+        if(other.gameObject.tag=="Ball"){
+            print("BallHit");
+            shield=false;
+            print(shield);
+            Destroy (other.gameObject);
+            StartCoroutine(shieldDelay());
+        }
+    }
+
+    IEnumerator shieldDelay() {
+     yield return new WaitForSeconds(5);
+
+        shield=true;
+        gameObject.GetComponent<AudioSource>().Play();
+
+            GameObject Containment = Instantiate(ContainmentPrefab, new Vector2(0,0), Quaternion.identity);
+ }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
