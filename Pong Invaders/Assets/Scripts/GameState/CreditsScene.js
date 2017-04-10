4a0620ec301e5b1b6ba50cd13859e1479ac0e3f9
Matthew Spedale <redage100@gmail.com>
2017-04-10 16:45:35 -0500

    Implemented Credits
    Added basic credits screen with scroll text and back button. To do: Exit
    screen w/ Esc key, revise scroll

---
+++ Pong Invaders/Assets/Scripts/GameState/CreditsScene.js
@@ -0,0 +1,80 @@
+﻿#pragma strict
+
+var speed = 0.1;
+var crawling = false;
+var creds;
+
+var menu : boolean;
+var guiSkin : GUISkin;
+var icon : Texture2D;
+
+function Start()
+{
+    // initialize text
+    var tc = GetComponent(GUIText);
+    creds = "Pong Invaders Credits:\n\n";
+    creds += "BitJunkie is...\n\n";
+	creds += "Lead Game Design:\nCameron Moore\n\n";
+	creds += "BitJunkie Team Leader:\nMatthew Spedale\n\n";
+    creds += "Programming:\nJonah Knickles\nCameron Moore\nMatthew Spedale\nJeffrey Tolliver\n\n";
+    creds += "Art:\nTatyana Lee\n\n";
+    creds += "Music:\nMatthew Spedale\nJeffrey Tolliver\n\n";
+    creds += "Sound Design:\nMatthew Spedale\n\n";
+    creds += "BitJunkie Logo Design:\nPatrick Richardson\n\n";
+    creds += "Created for CSC 4362 Spr 2017\n\n";
+    creds += "Thanks for playing!";
+	tc.text = creds;
+
+	menu = !menu;
+
+/*
+Jonah Knickles: Programmer
+
+Tatyana Lee (ART): Artist
+
+Cameron Moore (CSC): Lead Designer, Programmer
+
+Matthew Spedale (CSC): Team Leader, Sound & Music Designer
+
+Jeffrey Tolliver (CSC): Programmer, Sound & Music Designer
+
+Patrick Richardson: Bit Junkie Logo Designer
+*/
+
+     
+}
+
+function Update ()
+{
+    // sets x pos
+	gameObject.transform.position.x = .41;
+
+    if (!crawling)
+    {
+        return;
+    }
+
+    // moves text upward
+    transform.Translate(Vector3.up * Time.deltaTime * speed);
+
+
+    /*  Stops text at y position; implement individual line deletion later
+    if (gameObject.transform.position.y > .95)
+    {
+    	crawling = false;
+    }
+    */
+}
+
+function OnGUI () 
+{
+	GUI.skin = guiSkin;
+
+	if(menu)
+	{
+		if (GUI.Button (Rect (Screen.width/2 - 225, Screen.height/2 + 175, 90, 40), "Back")) 
+		{
+        	Application.LoadLevel("MainMenu");
+        }
+    }
+}
