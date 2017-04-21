33e8b31555f73bcc5cf38ee4f8bc7037eec9aa43
Mayor-Cam <cammoo37@gmail.com>
2017-04-21 00:10:29 -0500

    Invader X-movement working

--- Pong Invaders/Assets/Scripts/NPCs/Invaders/script_InvaderFleet1.cs
+++ Pong Invaders/Assets/Scripts/NPCs/Invaders/script_InvaderFleet1.cs
@@ -11,8 +11,8 @@ public class script_InvaderFleet1 : MonoBehaviour {
 	public float stepXsize = 0.25f;
 	float stepX = 0f;
 	public float stepYsize = 0.5f;
-	public float maxRest = 60;
-	float restTime = 60;
+	public int maxRest = 60;
+	public int restTime = 60;  // private
 	Vector2 currentPosition = new Vector2(0f,0f);
 	Vector2 newPosition = new Vector2(0f,0f);
 	Vector2 newVelocity = new Vector2(0f,0f);
@@ -22,7 +22,7 @@ public class script_InvaderFleet1 : MonoBehaviour {
 		fleetRB = GetComponent<Rigidbody2D>();
 		currentPosition = fleetRB.transform.position;
 		newPosition = currentPosition;
-		destination = fleetRB.transform.position;	
+		destination = fleetRB.transform.position;
 	}
 	
 	
@@ -31,51 +31,68 @@ public class script_InvaderFleet1 : MonoBehaviour {
 		newPosition = currentPosition;
 		newVelocity = new Vector2(0f,0f);
 		
-		if (restTime <= 0f)
+		// Not at rest
+		if (restTime <= 0)
 		{
-			// If-statement for determining whether next step will run into a wall
-			// If it does, make xDir *= -1, and do a StepY
-			// If not:
-			StepX();
+			// X and Y steps
+			if(restTime+1 > 0) 
+			{
+				// If-statement for determining whether next step will run into a wall
+				// If it does, make xDir *= -1, and do a StepY
+				// If not:
+				//StepX();
+				print("StepX");
+				destination.y = currentPosition.y;
+				destination.x = currentPosition.x + stepXsize * xDir;
+				
+				restTime--;
+			}
 			
 			// X movement
-			if (currentPosition.x * xDir < destination.x * xDir) 
+			if (currentPosition.x * xDir < destination.x *  xDir) // Not yet reached destination
 			{
-				newVelocity.x = stepSpeed * xDir;	
+				newVelocity.x = stepSpeed * xDir;
 			}
-			else 
+			else // Destination reached
 			{
 				newVelocity.x = 0f;
 				newPosition.x = destination.x;
-				restTime = maxRest;
+				restTime = maxRest;	
+				
+				print("X Destination reached");
 			}
-			
+				
 			// Y movement
 			if (currentPosition.y > destination.y)   // for InvaderFleet 2, switch to <
 			{
 				newVelocity.y = -stepSpeed;		// for Invaderfleet 2, make stepSpeed positive
 			}
-			else 
+			else // Destination reached
 			{
 				newVelocity.y = 0f;
 				newPosition.y = destination.y;
-				restTime = maxRest;
+				// restTime = maxRest;	
+				
+				print("Y Destination reached");
 			}	
-            
-			// Position and velocity update
-			fleetRB.transform.position = newPosition;
-			fleetRB.velocity = newVelocity;			
 		}
-		else
+		// At rest
+		else 
 		{
 			restTime -= 1;
-		}	
+		}
+		
+		// Position and velocity update
+		fleetRB.transform.position = newPosition;
+		fleetRB.velocity = newVelocity;
 	}
 	
+	/*
 	void StepX () {
 		destination.y = currentPosition.y;
 		destination.x = currentPosition.x + stepXsize * xDir;
 	}
+	*/
 	
 	void StepY () {
 		destination.y = currentPosition.y - stepYsize;  // change to "+ stepYsize" for InvaderFleet 2
