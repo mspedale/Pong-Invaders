2a99b9c2ee8856d59355b1408727dd1043f37123
Mayor-Cam <cammoo37@gmail.com>
2017-08-09 05:52:50 -0500

    Drone overhaul in progress

--- Pong Invaders/Assets/Scripts/NPCs/Drones/blueDrone.cs
+++ Pong Invaders/Assets/Scripts/NPCs/Drones/blueDrone.cs
@@ -4,36 +4,81 @@ using UnityEngine;
 
 public class blueDrone : MonoBehaviour {
     
+	GameObject droneFleet;	// parent parent droneFleet object
+	GameObject droneSlot;	// parent droneSlot object
 	public GameObject projectile;
     GameObject projectileClone;
 	public GameObject deathExplosion;
 	GameObject smallExplosionClone;
 	
+	public float speedX = 0.2f;
+	public float speedY = 0.5f;
+	
+	private Vector2 formPoint = new Vector2(0,0);  // Position in fleet formation
+	private float y1;  // This is the y position at which the drone maneuvers to match the x-position of its formPoint.  Set equal to the Y of the droneFleet object.  
+						// Should be set BEHIND mothership shield
     float t;
-    Vector3 newPosition;
+    Vector3 shotPosition;
+	Vector2 curPosition;
+	Vector2 newPosition;
 	
 	// Use this for initialization
 	void Start () 
 	{
 		t = Random.Range(0f, 3f);
+		droneSlot = transform.parent.gameObject;
+		formPoint = droneSlot.transform.position;
+		droneFleet = droneSlot.transform.parent.gameObject;
+		Vector2 fleetPosition = droneFleet.transform.position;
+		y1 = fleetPosition.y;
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
-    //fire weapons
-     newPosition = transform.position;
-    Vector3 eps = new Vector3(0f,.05f,0f);
-    newPosition = newPosition + eps;
-    
-    RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, Vector2.up); //raycast THROUGH the object and return an array of hits
-    if(hit[1].transform.gameObject.name!="obj_player") //If the first thing it hits that isn't itself's name is "obj_player"
-        {
-        if (Time.time - t > 3f)
+		//fire weapons  (Jonah's code.  Not sure wtf this does.)
+		 shotPosition = transform.position;
+		Vector3 eps = new Vector3(0f,.05f,0f);
+		shotPosition = shotPosition + eps;
+		
+		
+		// If in formation position, shoot
+		if (curPosition == formPoint)
+		{
+			// Check to see if the player is in front of the drone before shooting.  (Jonah's code)
+			RaycastHit2D[] hit = Physics2D.RaycastAll(transform.position, Vector2.up); //raycast THROUGH the object and return an array of hits
+			if(hit[1].transform.gameObject.name!="obj_player") //If the first thing it hits that isn't itself's name is "obj_player"
 			{
-                StartCoroutine(shoot());
-                t = Time.time;  
-            }
+				if (Time.time - t > 3f)
+					{
+						StartCoroutine(shoot());
+						t = Time.time;  
+					}
+			}
+		}
+		// If NOT in formation position, move to it
+		else 
+		{
+			newPosition = transform.position;
+			
+			// Algorithm for movement into position
+			// If not yet at horizontal movement point
+			if (transform.position.y < y1)   // for player 2:  if (transform.position.y > y1)
+			{
+				newPosition.y += speedY;	// for player 2: newPosition.y -= speedY;
+			}
+			// Horizontal movement
+			else 
+			{
+				float xDif = formPoint.x - transform.position.x;		// x-displacement from formPoint
+				// if not at formPoint x-position
+				if (xDif != 0)
+					newPosition.x += speedX * Mathf.Sign(xDif);	// move left/right to formPoint.  Use sign of xDif to indicate direction.
+				// if not at formPoint y-position, vertical movement
+				else if (transform.position.y < formPoint.y)	// for player 2:  else if (transform.position.y > formPoint.y)
+					newPosition.y += speedY;   // move to match formPoint.y
+			}
+			transform.position = newPosition;
 		}
 	}
 	
@@ -61,15 +106,33 @@ public class blueDrone : MonoBehaviour {
 		}
 		*/
 	}
+	
     IEnumerator shoot() //more firing
     {
         yield return new WaitForSeconds (3);
-        projectileClone = Instantiate(projectile, newPosition, Quaternion.identity) as GameObject;
+        projectileClone = Instantiate(projectile, shotPosition, Quaternion.identity) as GameObject;
         Destroy(projectileClone, 3);
     }
 	
 	void Destroy() {
-		smallExplosionClone = Instantiate(deathExplosion, newPosition, Quaternion.identity) as GameObject;
+		smallExplosionClone = Instantiate(deathExplosion, shotPosition, Quaternion.identity) as GameObject;
 		Destroy (gameObject);
+	}
+	
+	// Methods for telling the drone its formation position
+	void SetFormPoint(Vector2 newPoint) {
+		formPoint = newPoint;
+	}
+
+	void SetFormPoint(float x, float y) {
+		formPoint = new Vector2(x,y);
+	}
+
+	void SetFormPointX(float x) {
+		formPoint = new Vector2(x, formPoint.y);
+	}
+
+	void SetFormPointY(float y) {
+		formPoint = new Vector2(formPoint.x, y);
 	}
 }
--- Pong Invaders/Assets/Scripts/NPCs/Drones/droneFleetScript.cs
+++ Pong Invaders/Assets/Scripts/NPCs/Drones/droneFleetScript.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class droneFleetScript : MonoBehaviour {
 
+
+	private static Transform[] droneSlot;
+	private int slotCount = 0;
+	
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		// fill array with child slot objects
+		droneSlot = GetComponentsInChildren<Transform>(false);
 		
+		// Set slotCount
+		slotCount = gameObject.childCount;
 	}
 	
 	// Update is called once per frame
@@ -16,5 +25,17 @@ public class droneFleetScript : MonoBehaviour {
 		{
 			Destroy(gameObject);
 		}
+	}
+	
+	// Method for spawning a new fleet of drones
+	public void fleetSpawn() {
+		// determine which slots are inactive
+		// spawn drones according to a timer
+	}
+	
+	// Method for spawning a drone (for use in fleetSpawn()
+	private void droneSpawn() {
+		// spawn a drone
+		// set drone as child of chosen slot
 	}
 }
